ebb42c1062014ac079e5f0056bd7dc0b7e54a535
eashinji <mynamemdjihad@gmail.com>
2024-11-09 21:48:12 -0500

    Update WeatherApp launch.bat

--- WeatherApp/src/WeatherApp launch.bat
+++ WeatherApp/src/WeatherApp launch.bat
@@ -1,4 +1,4 @@
 @echo off
-cd /d "C:\Users\jihad\OneDrive - Wilfrid Laurier University\CP213\Weather\src"
-java -cp ".;C:\Users\jihad\OneDrive - Wilfrid Laurier University\CP213\Weather\gson-2.11.0.jar" WeatherApp
+cd /d "Paste YOUR WeatherApp.Java Adress"
+java -cp ".;Paste YOUR WeatherApp.Java Adress\gson-2.11.0.jar" WeatherApp
 pause
